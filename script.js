6fb5a5af5f0606c878b1ec2cdab8022a0da3f38b
Steve Chen <chensteve520@gmail.com>
2022-07-02 21:22:59 +0800

    Geolocation API

--- script.js
+++ script.js
@@ -27,11 +27,65 @@ let weather = {
 			"Humidity: " + humidity + "%";
 		document.querySelector(".wind").innerText =
 			"Wind speed: " + speed + " km/h";
-        document.querySelector(".weather").classList.remove("loading");
-        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name+ "')"
+		document.querySelector(".weather").classList.remove("loading");
+		document.body.style.backgroundImage =
+			"url('https://source.unsplash.com/1600x900/?" + name + "')";
 	},
 	search: function () {
 		this.fetchWeather(document.querySelector(".search-bar").value);
+	},
+};
+
+let geocode = {
+	reverseGeocode: function (latitude, longitude) {
+		var api_key = "Temp";
+		var api_url = "https://api.opencagedata.com/geocode/v1/json";
+		var request_url =
+			api_url +
+			"?" +
+			"key=" +
+			api_key +
+			"&q=" +
+			encodeURIComponent(latitude + "," + longitude) +
+			"&pretty=1" +
+			"&no_annotations=1";
+
+		var request = new XMLHttpRequest();
+		request.open("GET", request_url, true);
+
+		request.onload = function () {
+			if (request.status === 200) {
+				// Success!
+				var data = JSON.parse(request.responseText);
+				weather.fetchWeather(data.results[0].components.city);
+			} else if (request.status <= 500) {
+				// We reached our target server, but it returned an error
+				console.log(
+					"unable to geocode! Response code: " + request.status
+				);
+				var data = JSON.parse(request.responseText);
+				console.log("error msg: " + data.status.message);
+			} else {
+				console.log("server error");
+			}
+		};
+
+		request.onerror = function () {
+			// There was a connection error of some sort
+			console.log("unable to connect to server");
+		};
+
+		request.send(); // make the request
+	},
+	getLocation: function () {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(success, console.error);
+		} else {
+			weather.fetchWeather("Seattle");
+		}
+        function success(data) {
+			geocode.reverseGeocode(data.coords.latitude, data.coords.longitude);
+		}
 	},
 };
 
@@ -39,10 +93,10 @@ document.querySelector(".search").addEventListener("click", function () {
 	weather.search();
 });
 
-document.querySelector(".search-bar").addEventListener("keyup", function(){
-    if(event.key == "Enter"){
-        weather.search();
-    }
-})
+document.querySelector(".search-bar").addEventListener("keyup", function () {
+	if (event.key == "Enter") {
+		weather.search();
+	}
+});
 
-weather.fetchWeather("Seattle");
+geocode.getLocation();
